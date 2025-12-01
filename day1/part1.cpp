7da158a4df9629ecbdaa86edf8929fdc6c7d0c2c
Ava Cameron <avacameron6283@gmail.com>
2025-12-01 17:02:55 +1100

    day1 part1

---
+++ day1/part1.cpp
@@ -0,0 +1,30 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+auto main(int argc, char **argv) -> int {
+    if (argc != 2) {
+        std::cout << "usage: day1 INPUT_FILE" << std::endl;
+        return 1;
+    }
+
+    std::string path = argv[1];
+    std::ifstream file(path);
+
+    size_t password = 0;
+    int position = 50;
+    std::cout << "start: " << position << std::endl;
+
+    std::string line;
+    while (std::getline(file, line)) {
+        int sign = 2 * (line[0] == 'R') - 1;
+        int distance = atoi(line.substr(1).c_str());
+        int rotation = sign * distance;
+        position = (position + rotation + 100) % 100;
+        password += !position;
+        std::cout << rotation << " " << position << std::endl;
+    }
+
+    std::cout << "password: " << password << std::endl;
+}
